32463ba795cb6ec99202412e3184bb3ec990134e
kartikpathak1999 <ktpathak2871@gmail.com>
2023-11-18 07:06:30 +0530

    my first project

--- src/main/java/dev/farhan/movieist/movies/ReviewController.java
+++ src/main/java/dev/farhan/movieist/movies/ReviewController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 import java.util.Optional;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/reviews")
 public class ReviewController {
